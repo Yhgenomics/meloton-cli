47271aa64e913ea55bbfcb8e92641626ca4f17b4
Shubo Yang <yangshubo@outlook.com>
2016-01-20 17:14:18 +0800

    using new codes

--- 3rd/Logger.h
+++ 3rd/Logger.h
@@ -8,7 +8,8 @@
 #ifndef MRT_LOGER_H_
 #define MRT_LOGER_H_ 
 
-#include "stdio.h"
+#include <stdio.h>
+#include <iostream>
 #include <stdarg.h>
 #include "Timer.h"
 
@@ -17,55 +18,49 @@ class Logger
 public:
 
     template<typename ...Types>
-    static void sys( const char* fmt , Types... args )
+    static void sys( const char* fmt, Types... args)
     {
 
 #ifdef _WIN32
-        printf( "[SYS] %lld: " , Timer::tick( ) );
+        tprintf( "[SYS] %lld: ", Timer::tick() );
 #else
-        printf( "\033[1;33m[SYS] %zu: " , Timer::tick( ) );
+        tprintf( "\033[1;33m[SYS] %lld: ", Timer::tick() );
 #endif
 
-        printf( fmt , args... );
+        tprintf( fmt, args... );
 
 #ifdef _WIN32
-        printf( "\r\n" );
+        tprintf( "\r\n" );
 #else
-        printf( "\033[0m\r\n" );
+        tprintf( "\033[0m\r\n" );
 #endif      
 
     }
 
     template<typename ...Types>
-    static void error( const char* fmt , Types... args )
+    static void error( const char* fmt, Types... args )
     {
 #ifdef _WIN32
-        printf( "[ERROR] %lld: " , Timer::tick( ) );
+        tprintf( "[ERROR] %lld: ", Timer::tick() );
 #else
-        printf( "\033[0;31m[ERROR] %zu: " , Timer::tick( ) );
+        tprintf( "\033[0;31m[ERROR] %lld: ", Timer::tick() );
 #endif
-
-        printf( fmt , args... );
+        tprintf( fmt , args... );
 
 #ifdef _WIN32
-        printf( "\r\n" );
+        tprintf( "\r\n" );
 #else
-        printf( "\033[0;31m\r\n" );
+        tprintf( "\033[0;31m\r\n" );
 #endif    
     }
 
     template<typename ...Types>
-    static void log( const char* fmt , Types... args )
-    {
-#ifdef _WIN32
-        printf( "%lld: " , Timer::tick( ) );
-#else
-        printf( "%zu: " , Timer::tick( ) );
-#endif   
-        printf( fmt , args... );
-        printf( "\r\n" );
+    static void log( const char* fmt, Types... args )
+    { 
+        tprintf( "%lld: ", Timer::tick() );
+        tprintf( fmt , args... );
+        tprintf( "\r\n" );
     }
-
 private:
 
     Logger( )
@@ -74,6 +69,26 @@ private:
     ~Logger( )
     {
     };
+
+    static void tprintf( const char* format ) // base function
+    {
+        std::cout << format;
+    }
+
+    template<typename T , typename... Targs>
+    static void tprintf( const char* format , T value , Targs... Fargs ) // recursive variadic function
+    {
+        for ( ; *format != '\0'; format++ )
+        {
+            if ( *format == '%' )
+            {
+                std::cout << value;
+                tprintf( format + 1 , Fargs... ); // recursive call
+                return;
+            }
+            std::cout << *format;
+        }
+    }
 };
 
 #endif // !MRT_LOGER_H_ 
